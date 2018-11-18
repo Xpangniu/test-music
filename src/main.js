1f81b9ee97815afb19a9b5a7ae390adcc3283e8c
Xpangniu <631949505@qq.com>
2018-11-18 23:05:10 +0800

    add

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import VueLazyload from 'vue-lazyload'
+
+import App from './App'
+import store from './store'
+import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
+Vue.use(VueResource)
+Vue.use(VueLazyload, {
+    error: require('./assets/loading.svg'),
+    loading: require('./assets/loading.svg'),
+    attempt: 1
+  }
+)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  render: h => h(App)
+})
